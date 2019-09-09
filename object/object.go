f90588b2eba51f9683e3b33bd62e20b4ad15945a
Nikhil Thomas <nikhil@arceoanalytics.com>
2019-09-09 11:54:33 -0400

    Evaluator started
    Signed-off-by: Nikhil Thomas <nikhil@arceoanalytics.com>

---
+++ object/object.go
@@ -0,0 +1,59 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+type Null struct{}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
+}
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
+}
